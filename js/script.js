bab4b435d63dc390f4b1109358a16e11ba68ed89
Eric Nguyen <15enguyen4@gmail.com>
2022-08-31 17:12:58 -0700

    footer complete

--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+// function toggle() {
+//   var blur = document.getElementsByClassName('image-grid');
+
+//   for (var i = 0; i < image-grid.length; i++) {
+//     image-grid[i].classList.toggle('active');
+//   }
+
+//   var popup = document.getElementById('burger');
+//   popup.classList.toggle('active')
+// }
